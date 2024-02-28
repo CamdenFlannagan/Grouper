ab9192a0e394b46a1f0cd09b8aff1ea143bdfa60
AbigailSpencer <abigail.spencer@my.wheaton.edu>
2024-02-28 10:17:56 -0600

    The page won't load beyond the login screen so

--- frontend/src/components/CreateNewGroup.js
+++ frontend/src/components/CreateNewGroup.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import './CreateNewGroup.css'; 
 import { useNavigate } from 'react-router-dom';
+
 function CreateNewGroup() {
     const navigate = useNavigate();
+    const groupName = '';
+
 
     const createTask = async () => {
         navigate('/createnewgroup/createnewtask');
@@ -9,6 +13,11 @@ function CreateNewGroup() {
     return (
         <div className="CreateNewGroup">
             <h1>Create a new group here!</h1>
+            <input
+                type="text"
+                placeholder="Enter name"
+                value={groupName}
+            />
             <button onClick={createTask}>Make new task</button>
         </div>
     );
