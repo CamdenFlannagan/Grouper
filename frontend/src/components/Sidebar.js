1c76b82ef45360eac757e36749b44e1762b78e40
Abraham Austin <abeaustin25@gmail.com>
2024-04-10 11:56:45 -0500

    Nate Toflin's code. He could not push his chnages due to some github changes.

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import './Sidebar.css';
+
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
+
 
   return (
     <>
@@ -18,16 +21,16 @@ const Sidebar = () => {
         <button className="close-button" onClick={toggleSidebar}>X</button>
         <Link to="/createnewgroup" className="sidebar-item profile">Create Group</Link>
         <Link to="/createnewtask" className="sidebar-item">Create Task</Link>
-        <Link to="#join-group" className="sidebar-item">Join Group</Link>
+        <Link to="/browse" className="sidebar-item">Browse Groups</Link>
         <Link to="#your-groups" className="sidebar-item">Your Groups</Link>
         <Link to="#settings" className="sidebar-item">Settings</Link>
+        <Link to="/home" className="sidebar-item">Logout</Link>
       </div>
     </>
   );
 };
 
 
+
+
 export default Sidebar;
-
-
-
