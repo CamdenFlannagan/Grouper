d8220e25c76844f764db52ab65e6cb53d1a29be5
AbigailSpencer <abigail.spencer@my.wheaton.edu>
2024-04-10 09:34:20 -0500

    Updated groups prototype

--- frontend/src/components/Groups.js
+++ frontend/src/components/Groups.js
@@ -3,25 +3,26 @@ import { useNavigate } from 'react-router-dom';
 import './Groups.css';
 
 // Groups/dashboard is a big work in progress
-
-function Groups() {
-    const navigate = useNavigate();
-    const Groups = ({ className, ...props }) => {
+export const Groups = ({ className, ...props }) => {
     return (
-        <div className={"groups " + className}>
+      <div className={"groups " + className}>
+        <div className="rectangle-3"></div>
         <div className="upcoming-tasks">UPCOMING TASKS </div>
         <div className="task-1-group-due-date">
-            <span>
+          <span>
             <span className="task-1-group-due-date-span">
-                Task #1
-                <br />
+              Task #1
+              <br />
             </span>
             <span className="task-1-group-due-date-span2">Group, Due Date</span>
-            </span>{" "}
+          </span>{" "}
         </div>
-        </div>
+        <div className="rectangle-1"></div>
+        <div className="rectangle-2"></div>
+        <div className="groups2">GROUPS </div>
+        <div className="art-101">Art 101 </div>
+        <div className="comm-101">Comm 101 </div>
+      </div>
     );
-    };
-}
-
-export default Groups;
+};
+  
