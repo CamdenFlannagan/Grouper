ef33c073e54aa27d4e1c6b9b9b2aa61a439a3c26
Francisco RS <yahir.rodsan@gmail.com>
2024-04-24 00:35:53 -0500

    Add icons and overall fix sidebar menu design UI.

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Sidebar.css';
 import { signOut } from 'firebase/auth';
-import { auth } from '../firebase'; 
+import { auth } from '../firebase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faUsers, faHome, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
 
-  
   const handleLogout = async () => {
     try {
       await signOut(auth);
@@ -29,11 +30,18 @@ const Sidebar = () => {
       </button>
       <div className={`sidebar ${isOpen ? '' : 'closed'}`}>
         <button className="close-button" onClick={toggleSidebar}>X</button>
-        <Link to="/createnewgroup" className="sidebar-item profile">Create Group</Link>
-        <Link to="/browse" className="sidebar-item">Browse Groups</Link>
-        <Link to="/dashboard" className="sidebar-item">Dashboard</Link>
-        <Link to="#settings" className="sidebar-item">Settings</Link>
-        <a href="#" onClick={handleLogout} className="sidebar-item">Logout</a>
+        <Link to="/createnewgroup" className="sidebar-item profile">
+          <FontAwesomeIcon icon={faPlus} className="icon" />Create Group
+        </Link>
+        <Link to="/browse" className="sidebar-item">
+          <FontAwesomeIcon icon={faUsers} className="icon" />Browse Groups
+        </Link>
+        <Link to="/dashboard" className="sidebar-item">
+          <FontAwesomeIcon icon={faHome} className="icon" />Dashboard
+        </Link>
+        <a href="#" onClick={handleLogout} className="sidebar-item">
+          <FontAwesomeIcon icon={faSignOutAlt} className="icon" />Logout
+        </a>
       </div>
     </>
   );
